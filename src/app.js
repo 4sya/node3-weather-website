608f9497cd2ab5d05275afd2021cb49d8f636b7f
Portia Tanaval <pamelaportiatanaval@gmail.com>
2020-11-18 11:12:16 +0800

    Init commit

---
+++ src/app.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const request = require("postman-request");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const app = express();
+
+//defines path for express config
+const publicDIRPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+hbs.registerPartials(partialsPath);
+
+//Setup handlers bar engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+// setup static dir to serve
+app.use(express.static(publicDIRPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Pam Tanaval",
+    msg: "Use this site to get your weather!",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Pam Tanaval",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    msg: "This is some helpful text.",
+    name: "Pam Tanaval",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Pam Tanaval",
+    errorMsg: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
+
+// app.com  , app.com/help, app.com/about
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=0e14dcdfb76f4ee50a32a4119b298d54&query=${latitude},${longitude}&units=f`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions}.It is currently ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees out`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
